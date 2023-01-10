e4f558c03db4508a61cd9aee118b18c813ca970e
Abula <nikaabuladze28@gmail.com>
2023-01-11 01:56:53 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="something">
+    <HeaderComponent />
+
+</div>
+</template>
+
+  <script setup>
+  import HeaderComponent from './components/header.vue'
+
+  </script>
+
+<style scoped>
+.something{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 1024px) {
+  
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="counter">
+    <p :class="getData">{{ numberVariable }}</p>
+    <div class="btns">
+        <button class="decBtn" @click="numberVariable--">-</button>
+        <button class="incBtn" @click="numberVariable++">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      redClass: "test",
+      greenClass: "test2",
+      numberVariable: 0,
+      textValue: null,
+
+      myArr: ["ZD banda", "baro lasha", "chemi venebi", "rasqvia bicho", 100],
+    };
+  },
+  mounted() {
+    this.clickFunction();
+  },
+
+  created() {
+    console.log("created mode");
+  },
+
+  watch: {
+    numberVariable: {
+      immediate: true,
+      handler: function () {
+        console.log("var changed");
+      },
+    },
+  },
+
+  computed: {
+    getData() {
+      if (this.numberVariable < 0) {
+        return this.redClass;
+      } else if (this.numberVariable > 0) {
+        return this.greenClass;
+      } else {
+        return;
+      }
+    },
+  },
+
+  methods: {
+    clickFunction() {
+      console.log(this.myArr);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.counter{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.decBtn{
+    background-color: red;
+}
+
+.incBtn{
+    background-color: green;
+}
+
+.btns button{
+    margin: 10px 10px 10px;
+    padding: 10px 45px;
+    font-size: 25px;
+    font-weight: bold;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    color: white;
+
+}
+
+.counter p{
+    font-size: 35px;
+    font-weight: bold;
+}
+
+.test {
+  color: red;
+}
+
+.test2 {
+  color: green;
+}
+
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: red;
+}
+
+.dark {
+  background-color: black;
+}
+</style>
